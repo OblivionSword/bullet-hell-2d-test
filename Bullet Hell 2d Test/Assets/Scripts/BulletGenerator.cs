ad95d3678eab0f8fe7aa277c4653c590d1e37ed7
OblivionSword <hunter140797@gmail.com>
2022-03-04 18:14:26 +0100

    added bullet generator

---
+++ Bullet Hell 2d Test/Assets/Scripts/BulletGenerator.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletGenerator : MonoBehaviour
+{
+    public int numberOfColumns;
+    public float speed;
+    public Sprite texture;
+    public Color color;
+    public float lifetime;
+    public float firerate;
+    public float size;
+    private float angle;
+    public Material material;
+    public float spinSpeed;
+    private float time;
+
+    public ParticleSystem system;
+
+    private void Awake()
+    {
+        GenerateBullets();
+    }
+
+    private void FixedUpdate()
+    {
+        time += Time.fixedDeltaTime;
+        
+        transform.rotation = Quaternion.Euler(0,0,time * spinSpeed);
+    }
+
+    void GenerateBullets()
+    {
+        angle = 360f / numberOfColumns;
+
+        for (int i = 0; i < numberOfColumns; i++)
+        {
+            // A simple particle material with no texture.
+            Material particleMaterial = material;
+
+            // Create a green Particle System.
+            var go = new GameObject("Particle System");
+            go.transform.Rotate(angle * i, 90, 0); // Rotate so the system emits upwards.
+            go.transform.parent = this.transform;
+            go.transform.position = this.transform.position;
+            system = go.AddComponent<ParticleSystem>();
+            go.GetComponent<ParticleSystemRenderer>().material = particleMaterial;
+            var mainModule = system.main;
+            mainModule.startColor = Color.green;
+            mainModule.startSize = 0.5f;
+            mainModule.startSpeed = speed;
+            mainModule.maxParticles = 100000;
+            mainModule.simulationSpace = ParticleSystemSimulationSpace.World;
+
+            var emission = system.emission;
+            emission.enabled = false;
+
+            var shape = system.shape;
+            shape.enabled = true;
+            shape.shapeType = ParticleSystemShapeType.Sprite;
+            shape.sprite = null;
+
+            var text = system.textureSheetAnimation;
+            text.mode = ParticleSystemAnimationMode.Sprites;
+            text.AddSprite(texture);
+            text.enabled = true;
+        }
+
+        // Every 2 secs we will emit.
+        InvokeRepeating("DoEmit", 0f, firerate);
+    }
+
+    void DoEmit()
+    {
+        foreach (Transform child in transform)
+        {
+            system = child.GetComponent<ParticleSystem>();
+            // Any parameters we assign in emitParams will override the current system's when we call Emit.
+            // Here we will override the start color and size.
+            var emitParams = new ParticleSystem.EmitParams();
+            emitParams.startColor = color;
+            emitParams.startSize = size;
+            emitParams.startLifetime = lifetime;
+            system.Emit(emitParams, 10);
+        }
+    }
+}
