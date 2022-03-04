663a2277c28b9cd74024ef27ddc2617b58092b0c
OblivionSword <hunter140797@gmail.com>
2022-03-04 20:57:02 +0100

    added player movement and boundaries

---
+++ Bullet Hell 2d Test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+
+    private Vector2 moveDirection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void ProcessInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
