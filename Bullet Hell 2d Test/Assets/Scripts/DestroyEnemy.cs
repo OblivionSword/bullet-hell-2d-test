1b9a0459bf8a8689b895f7c1ba1f3fe17d09027d
OblivionSword <hunter140797@gmail.com>
2022-03-11 18:31:41 +0100

    added enemy health bar

---
+++ Bullet Hell 2d Test/Assets/Scripts/DestroyEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemy : MonoBehaviour
+{
+    public int attackDamage = 1;
+    private GameObject enemy;
+    private EnemyHealth enemyHealth;
+
+    private void Awake()
+    {
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        enemyHealth = enemy.GetComponent<EnemyHealth>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == enemy)
+        {
+            enemyHealth.TakeDamage(attackDamage);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Bullet Hell 2d Test/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int startingHealth;
+    public int currentHealth;
+    public Slider healthSlider;
+
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+}
